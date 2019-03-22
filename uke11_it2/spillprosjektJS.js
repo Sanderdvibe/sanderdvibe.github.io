56a0fa268e6c591d7b0ef7aa717d2d7275f4d207
Sanderdvibe <sanderh2604@hotmail.com>
2019-03-22 13:35:25 +0100

    commit file

--- uke11_it2/spillprosjektJS.js
+++ uke11_it2/spillprosjektJS.js
@@ -29,7 +29,7 @@ var endreImg = function (img) {
 var endreButtons = function (buttonList) {
     buttonBox.innerHTML = "";
     for (var i = 0; i < buttonList.length; i++) {
-        buttonBox.innerHTML += "<buttononClick="+buttonList[i][1]+">" + buttonList[i][0] + "</button>"
+        buttonBox.innerHTML += "<button onClick="+buttonList[i][1]+">" + buttonList[i][0] + "</button>"
     }
 };
 
@@ -41,3 +41,46 @@ var advanceTo = function (s) {
 };
 
 //Til sist skal jeg lage objektet som inneholder hvert eneste scenario, desto flere scenarioer jeg lager, desto flere valgmuligheter får jeg
+var scenario = {
+    one: {
+        image: "",
+        text: "You find yourself covered in blood with a massive headache. You have no clue of what has happened. When you sit up and turn around you see a bloody axe and some bloody trails. These trails lead straight into the dark forest ahead.",
+    },
+    two: {
+        image: "",
+        text: "Karakter navn, do you want to proceed into the forest following the trails?",
+        buttons: [["Run away from the forest", "advanceTo(scenario.three)"], ["Follow the bloody trails", "advanceTo(scenario.four)"]]
+    },
+    three: {
+        image: "",
+        text: "You run away from the forest, desperately trying to figure out what happened. Suddenly a pack of wild dogs seems to caught your smell, and your only option is to run back into the forest.",
+        buttons: [["Continue", "advanceTo(scenario.four)"]]
+    },
+    four: {
+        image: "",
+        text: "The dark forest overwhelms you as you run in. The sound of barking dogs leave your area, and you start slowing down. You look behind you to see if any dogs followed your path, but everyone's gone. As you turn around a scream fills the forest.",
+        buttons: [["Follow the scream", "advanceTo(scenario.five_2)"], ["Follow the bloody trails", "advanceTo(scenario.five)"]]
+    },
+    five: {
+        images: "",
+        text: "You follow the bloody trails deeper into the forest. A cabin appears in the distance, it's hidden in a dense forest landscape. The trails lead directly at it and as you get closer you see that the door is open.",
+        buttons: [["Enter the cabin without thinking", "advanceTo(scenario.six)"], ["Approach the cabin slowly", "advanceTo(scenario.six_2)"]]
+    },
+    five_2: {
+        images: "",
+        text: "",
+        buttons: []
+    },
+    six: {
+        images: "",
+        text: "The stench of rotten blood fills your nose. The door at the other end of the hallway leads outside, that door is open as well. It doesn't seem like there are other rooms than this hallway... ",
+        buttons: [["Go out through the door at the end of the hallway", "advanceTo(scenario.seven)"]]
+    },
+    six_2: {
+        images: "",
+        text: "",
+        buttons: []
+    }
+};
+
+advanceTo(scenario.one);
